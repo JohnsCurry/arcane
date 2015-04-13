4e4b6cf63aeadb9ad6f625873287f0593a603ab9
John Curry <john.steven.curry@gmail.com>
2015-04-12 20:26:30 -0700

    added test to skill spec forgot to change branches

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -6,5 +6,9 @@ describe Skill do
   it { should validate_presence_of(:url)}
   it { should validate_presence_of(:description)}
   it { should have_many(:reviews).order("created_at DESC")}
+
+  describe "#average_rating" do
+    it "calculates the average"
+  end
   
 end
